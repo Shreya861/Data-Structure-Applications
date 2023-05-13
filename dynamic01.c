a64380dec141fbf2cfcac13c4414d068b4837bf6
Shreya Shanbhag <105070711+Shreya861@users.noreply.github.com>
2023-05-13 23:43:39 +0530

    Add files via upload

---
+++ dynamic01.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    int a[50][50],n;
+    printf("enter n value:\n");
+    scanf("%d",&n);
+    printf("enter the adjacency matrix:\n");
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    warshalls(a,n);
+    printf("all pair shortest path:\n");
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+}
+void warshalls(int a[50][50],int n)
+{
+  for(int k=0;k<n;k++)
+  {
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(a[i][k]==1 &&a[k][j]==1)
+            {
+                a[i][j]=1;
+            }
+        }
+    }
+  }
+}
